984a069687e7a2239593d188527e1ade2bb9a1a0
deepu <prashanthrmadi@gmail.com>
2013-06-19 01:14:37 -0500

    It has Basic Functionality to track the car

---
+++ src/com/prashanth/whereismycar/LocaterMapActivity.java
@@ -0,0 +1,258 @@
+package com.prashanth.whereismycar;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class LocaterMapActivity extends Activity implements
+		ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+
+	private GoogleMap mMap;
+	private LocationManager locationManager;
+	private Location location = null;
+	private Button btnStoreCarLocation = null;
+	private Button findCar = null;
+	Context context;
+	private LatLng carPosition = null;
+	private LocationClient mLocationClient = null;
+	private LocationRequest mLocationRequest = null;
+	private static final int MILLISECONDS_PER_SECOND = 1000;
+	public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+	private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND
+			* UPDATE_INTERVAL_IN_SECONDS;
+	private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+	private static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND
+			* FASTEST_INTERVAL_IN_SECONDS;
+
+	/*
+	 * Define a request code to send to Google Play services This code is
+	 * returned in Activity.onActivityResult
+	 */
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mycarlocator);
+		context = this;
+		btnStoreCarLocation = (Button) findViewById(R.id.storeCarLocation);
+		findCar = (Button) findViewById(R.id.findmycar);
+
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(
+				R.id.mapview)).getMap();
+		mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+		mMap.setMyLocationEnabled(true);
+
+		mLocationClient = new LocationClient(this, this, this);
+		if (servicesConnected()) {
+			mLocationRequest = LocationRequest.create();
+			mLocationRequest
+					.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+			mLocationRequest.setInterval(UPDATE_INTERVAL);
+			mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+		} else {
+			Toast.makeText(context, "Position Unavailable", Toast.LENGTH_SHORT)
+					.show();
+		}
+
+		// currentlatlang = new LatLng(29.528522, -98.57837);
+
+		btnStoreCarLocation.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				location = getCurrentLocation();
+				if (location != null) {
+					carPosition = convertLocationtoLatLang(location);
+					addMarker(carPosition, "CAR");
+				}
+			}
+		});
+
+		findCar.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				takeToLocation(carPosition);
+			}
+		});
+
+	}
+
+	/*
+	 * Called when the Activity becomes visible.
+	 */
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mLocationClient.connect();
+	}
+
+	/*
+	 * Called when the Activity is no longer visible.
+	 */
+	@Override
+	protected void onStop() {
+
+		if (mLocationClient.isConnected()) {
+			mLocationClient.removeLocationUpdates(this);
+		}
+		mLocationClient.disconnect();
+		super.onStop();
+	}
+
+	public void addMarker(LatLng position, String title) {
+		mMap.addMarker(new MarkerOptions().position(position).title(title));
+	}
+
+	/**
+	 * gets the current location details
+	 * 
+	 * @return
+	 */
+	private Location getCurrentLocation() {
+		Location location = mLocationClient.getLastLocation();
+		if (location != null) {
+			return location;
+		} else {
+			Toast.makeText(context, "Current Location Unavailable",
+					Toast.LENGTH_SHORT).show();
+			checkforGPSAndPromtOpen();
+			return null;
+		}
+	}
+
+	/**
+	 * Check if enabled and if not send user to the GSP settings Better solution
+	 * would be to display a dialog and suggesting to go to the settings
+	 */
+	private void checkforGPSAndPromtOpen() {
+		boolean enabled = locationManager
+				.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		if (!enabled) {
+			Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+			startActivity(intent);
+		}
+	}
+
+	/**
+	 * animates and takes to the location passed as paramter
+	 * 
+	 * @param toBeLocationLatLang
+	 */
+	public void takeToLocation(LatLng toBeLocationLatLang) {
+		if (toBeLocationLatLang != null) {
+			CameraUpdate update = CameraUpdateFactory.newLatLngZoom(
+					toBeLocationLatLang, 16);
+			mMap.animateCamera(update);
+		} else {
+			Toast.makeText(context, "Position Unavailable", Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+	/**
+	 * converts location to lat lang
+	 * 
+	 * @param location
+	 * @return
+	 */
+	private LatLng convertLocationtoLatLang(Location location) {
+		LatLng currentLatLang = new LatLng(location.getLatitude(),
+				location.getLongitude());
+
+		return currentLatLang;
+
+	}
+
+	/**
+	 * Handle results returned to the FragmentActivity by Google Play services
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case CONNECTION_FAILURE_RESOLUTION_REQUEST:
+			switch (resultCode) {
+			case Activity.RESULT_OK:
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Check that Google Play services is available
+	 * 
+	 * @return
+	 */
+	private boolean servicesConnected() {
+		int resultCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(this);
+		if (ConnectionResult.SUCCESS == resultCode) {
+			Log.d("Location Updates", "Google Play services is available.");
+			return true;
+		} else {
+			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+					resultCode, this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+			if (errorDialog != null) {
+				errorDialog.show();
+			}
+			return false;
+		}
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		mLocationClient.requestLocationUpdates(mLocationRequest, this);
+		location = getCurrentLocation();
+		takeToLocation(convertLocationtoLatLang(location));
+
+	}
+
+	@Override
+	public void onDisconnected() {
+
+	}
+
+	/**
+	 * Define the callback method that receives location updates
+	 * 
+	 * @param location
+	 */
+	@Override
+	public void onLocationChanged(Location location) {
+		// Report to the UI that the location was updated
+		String msg = "Updated Location: "
+				+ Double.toString(location.getLatitude()) + ","
+				+ Double.toString(location.getLongitude());
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+	}
+
+}
